bf65711ec488666b30a1d270fa2db6b6b849312c
Rits0631 <94890637+Ritesh0604@users.noreply.github.com>
2024-06-17 11:19:27 +0530

    Add app controllers for client, company, invoice, lead, and offer
    - Implemented controller for client management
    - Added controller for company operations
    - Developed controller for handling invoices
    - Created controller for lead management
    - Introduced controller for managing offers

---
+++ backend/src/controllers/appControllers/companyController/remove.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+
+const Client = mongoose.model('Client');
+const People = mongoose.model('People');
+
+const remove = async (Model, req, res) => {
+    const { id } = req.params;
+
+    const client = await Client.findOne({
+        company: id,
+        removed: false,
+    }).exec();
+
+    if (client) {
+        return res.status(400).json({
+            success: false,
+            result: null,
+            message: 'Cannot delete company if company attached to any people or he/she is client',
+        });
+    }
+
+    const people = await People.findOne({
+        company: id,
+        removed: false,
+    }).exec();
+    
+    if (people) {
+        return res.status(400).json({
+            success: false,
+            result: null,
+            message: 'Cannot delete company if company attached to any people or he/she is client',
+        });
+    }
+
+    const result = await Model.findOneAndUpdate(
+        { _id: id, removed: false },
+        {
+            $set: {
+                removed: true,
+            },
+        },
+    ).exec();
+
+    if (!result) {
+        return res.status(404).json({
+            success: false,
+            result: null,
+            message: 'No people found by this id: ' + id,
+        });
+    }
+    return res.status(200).json({
+        success: true,
+        result,
+        message: 'Successfully Deleted the people by id: ' + id,
+    });
+};
+
+module.exports = remove;
---
+++ backend/src/controllers/appControllers/companyController/update.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const Client = mongoose.model('Client');
+const Lead = mongoose.model('People');
+
+const update = async (Model, req, res) => {
+    req.body.removed = false;
+    const result = await Model.fineOneAndUpdate(
+        {
+            _id: req.params.id,
+            removed: false,
+        },
+        req.body,
+        {
+            new: true,
+            runValidators: true,
+        },
+    ).exec();
+
+    if (!result) {
+        return res.status(404).json({
+            success: false,
+            result: null,
+            message: 'No document found',
+        });
+    } else {
+        await Client.findOneAndUpdate(
+            { company: result._id },
+            { name: result.name },
+            { new: true },
+        ).exec();
+
+        await Lead.findOneAndUpdate(
+            { company: result._id },
+            { name: result.name },
+            { new: true },
+        ).exec();
+
+        return res.status(200).json({
+            success: true,
+            result,
+            message: 'We update this document ',
+        });
+    }
+};
+
+module.exports = update;
